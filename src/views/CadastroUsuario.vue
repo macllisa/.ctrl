b14f5f943b91961e5d518a69b77d6b87905eea1b
Lucca Romaniello <luccaromaniello1@gmail.com>
2019-09-18 00:32:56 -0300

    add cadastro usuario no firebase com mock

---
+++ src/views/CadastroUsuario.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cadastroUsuario">
+    <input type="email" v-model="email" placeholder="Email"> <br>
+    <input type="password" v-model="senha" placeholder="Senha"> <br>
+    <button @click.prevent="cadastrarUsuario()">Cadastrar</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import firebase from 'firebase'
+
+export default {
+  name: 'cadastroUsuario',
+  data() {
+    return {
+      email: '',
+      senha: ''
+    }
+  },
+  methods: {
+	cadastrarUsuario() {
+		firebase.auth().createUserWithEmailAndPassword(this.email, this.senha).then(
+			(user) => {
+				console.log("Usuário cadastrado com sucesso: ", user); /* eslint-disable-line no-console */
+				// TODO alert avisando o usuário
+			},
+			(error) => {
+				console.log("Error ao cadastrar o usuário: ", error); /* eslint-disable-line no-console */
+				// TODO alert avisando o usuário
+			}
+		)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CadastroUsuario />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import CadastroUsuario from './CadastroUsuario.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    CadastroUsuario
   }
 }
 </script>
