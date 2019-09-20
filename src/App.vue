8ac6f50ab6dd29b4ece158c9025bd0e18c3f227e
maria <mclima1999@gmail.com>
2019-09-19 22:36:50 -0300

    Adequando template

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,8 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESoooooIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-      <CadastroUsuario /> 
-    </v-content>
-  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-import CadastroUsuario from './views/CadastroUsuario.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    CadastroUsuario
-  },
-  data: () => ({
-    //
-  }),
-};
+
+}
 </script>
